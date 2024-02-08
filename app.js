d8544c64d7932b25998586e6dd61d6513f05c632
Enrique Graniel <legraniel.91@gmail.com>
2024-02-08 17:30:07 -0600

    project end

---
+++ app.js
@@ -0,0 +1,30 @@
+const API_KEY = "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"
+
+const submitIcon = document.querySelector("#submit-icon")
+const inputElement = document.querySelector("input")
+
+const getImages = async ()=>{
+    const options = {
+        method: "POST",
+        headers:{
+            "Authorization": `Bearer ${API_KEY}`,
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+            "prompt": inputElement.value,
+            "n": 4,
+            "size": "1024x1024"
+        })
+    }
+    console.log(options)
+    try{
+      const response = await fetch('https://api.openai.com/v1/images/generations', options)
+      const data = await response.json()
+      console.log(data)
+    }
+    catch (error){
+        console.error(error)
+    }
+}
+
+submitIcon.addEventListener('click', getImages)
